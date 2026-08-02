c5cb611431b23eff5624ed117f7c239522ec7278
Chandrap199 <76438546+Chandrap199@users.noreply.github.com>
2026-08-02 16:30:52 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const slides = document.querySelectorAll(".hero-slide");
+
+    let currentSlide = 0;
+
+    function showSlide(index){
+
+        slides.forEach((slide)=>{
+            slide.classList.remove("active");
+        });
+
+        slides[index].classList.add("active");
+
+    }
+
+    setInterval(()=>{
+
+        currentSlide++;
+
+        if(currentSlide >= slides.length){
+
+            currentSlide = 0;
+
+        }
+
+        showSlide(currentSlide);
+
+    },5000);
+
+});
