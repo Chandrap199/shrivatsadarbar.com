1c4aef5da2d0453f77957b8b69422dccfe2bf259
Chandrap199 <76438546+Chandrap199@users.noreply.github.com>
2026-08-09 22:23:21 +0530

    Update supabase-auth.js

--- supabase/supabase-auth.js
+++ supabase/supabase-auth.js
@@ -1,1 +1,12 @@
+// ============================================
+// ShriVatsaDarbar — Supabase Authentication
+// ============================================
 
+const SUPABASE_URL = "https://yqzolpymrwzcwbkykrq.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Zj1lpCfeU-nPv5VdsedWeA_MfKpDmIf";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
